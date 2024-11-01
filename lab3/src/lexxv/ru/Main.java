1b859a0a6b5bfe6e2f0b98c8f7b075455cdd7b8a
Alexey Vorobyev <a.vorobyev@winsolutions.ru>
2024-11-01 17:46:59 +0300

    lab3

---
+++ lab3/src/lexxv/ru/Main.java
@@ -0,0 +1,10 @@
+package lexxv.ru;
+import lexxv.ru.NonBlockReceive.FilteringAndProbe;
+import lexxv.ru.NonBlockReceive.TestReceive;
+
+public class Main {
+    public static void main(String[] args) {
+        FilteringAndProbe.start(args);
+//        TestReceive.startTest(args);
+    }
+}
